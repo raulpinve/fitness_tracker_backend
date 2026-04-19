b68fbff10d6ef0777f5aa09bdee852d9a8f897fe
Raúl Velásquez Pinto <raulvelasquezpinto@gmail.com>
2026-04-18 22:07:02 -0500

    /api added to static files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); 
